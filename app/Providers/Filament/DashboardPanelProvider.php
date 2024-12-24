8e86c8477ab1f45528a8a5fba782eb03972fb4ea
Wakhid Wicaksono <wichaksono.w@gmail.com>
2024-12-25 02:09:07 +0700

    update

--- app/Providers/Filament/DashboardPanelProvider.php
+++ app/Providers/Filament/DashboardPanelProvider.php
@@ -37,7 +37,6 @@ class DashboardPanelProvider extends PanelProvider
             ->id('dashboard')
             ->path('dashboard')
             ->login()
-            ->registration()
             ->colors([
                 'primary' => Color::Amber,
             ])
@@ -49,7 +48,6 @@ class DashboardPanelProvider extends PanelProvider
             ->discoverWidgets(in: app_path('Filament/Widgets'), for: 'App\\Filament\\Widgets')
             ->widgets([
                 Widgets\AccountWidget::class,
-                Widgets\FilamentInfoWidget::class,
             ])
             ->middleware([
                 EncryptCookies::class,
